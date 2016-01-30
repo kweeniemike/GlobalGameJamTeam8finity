5addf72d249ac54ce91d44012d58b596a0149b6f
Chi Gia Nguyen <gnguyen1011@gmail.com>
2016-01-30 17:54:44 +0100

    WakeUpAnimation
    also just a little start for the corsair

---
+++ Unity/GlobalGameJam16Team8Fintity/Assets/Scripts/PositionFixScript.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityStandardAssets.Characters.FirstPerson; 
+
+public class PositionFixScript : MonoBehaviour {
+
+	public FirstPersonController charscr;
+	public Vector3 pos;
+	public Vector3 RotVector;
+	private Quaternion rot;
+	public bool trigger;
+	public Animator anim;
+	public GameObject parent;
+	public Transform setTrans;
+	// Use this for initialization
+
+	void Awake()
+	{
+		charscr.enabled = false;
+	}
+	void Start () {
+	
+		parent = transform.parent.gameObject;
+		anim = parent.GetComponent<Animator>();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (charscr.isActiveAndEnabled && trigger)
+		{
+			transform.parent = null;
+			
+			trigger = false;
+		}
+	}
+
+
+
+
+	
+	
+
+
+
+}
